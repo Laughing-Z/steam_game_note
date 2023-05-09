ae15f738d7115d07d87a3d4fb4a66ce21231d7c3
Laughing-Z <1527711821@qq.com>
2023-05-10 02:48:25 +0800

    Add a cli interface

---
+++ cli.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+from getfriend import *
+import argparse
+
+def main():
+    print("Start")
+    parser = argparse.ArgumentParser(description='Steam Interface Command')
+    parser.add_argument("--friend",action="store_true",help="Print the Steam Online Friend List")
+
+    options = parser.parse_args()
+    steam_api = SteamAPI(STEAM_API_KEY)
+    friend_info = SteamFriendInfo(STEAM_UID,steam_api).get_friends_info()
+    if options.friend:
+        table = SteamFriendInfo(STEAM_UID,steam_api).get_table(friend_info)
+        console = Console()
+        console.print(table)
+
+    #table = SteamFriendInfo(STEAM_UID,STEAM_API_KEY).get_table(friend_info)
+
+if __name__ == "__main__":
+    main()
